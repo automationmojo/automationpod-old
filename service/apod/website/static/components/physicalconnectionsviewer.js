a6296b56686728ee565e9f879b430df0385f5183
Myron Walker <myron.walker@gmail.com>
2021-11-12 11:15:42 -0800

    Got the ports showing up and partial creating of links.

--- service/apod/website/static/components/physicalconnectionsviewer.js
+++ service/apod/website/static/components/physicalconnectionsviewer.js
@@ -1,4 +1,95 @@
 
+// This custom-routing Link class tries to separate parallel links from each other.
+// This assumes that ports are lined up in a row/column on a side of the node.
+function CustomLink() {
+    go.Link.call(this);
+    };
+    go.Diagram.inherit(CustomLink, go.Link);
+
+    CustomLink.prototype.findSidePortIndexAndCount = function(node, port) {
+    var nodedata = node.data;
+    if (nodedata !== null) {
+        var portdata = port.data;
+        var side = port._side;
+        var arr = nodedata[side + "Array"];
+        var len = arr.length;
+        for (var i = 0; i < len; i++) {
+        if (arr[i] === portdata) return [i, len];
+        }
+    }
+    return [-1, len];
+    };
+
+    CustomLink.prototype.computeEndSegmentLength = function(node, port, spot, from) {
+    var esl = go.Link.prototype.computeEndSegmentLength.call(this, node, port, spot, from);
+    var other = this.getOtherPort(port);
+    if (port !== null && other !== null) {
+        var thispt = port.getDocumentPoint(this.computeSpot(from));
+        var otherpt = other.getDocumentPoint(this.computeSpot(!from));
+        if (Math.abs(thispt.x - otherpt.x) > 20 || Math.abs(thispt.y - otherpt.y) > 20) {
+        var info = this.findSidePortIndexAndCount(node, port);
+        var idx = info[0];
+        var count = info[1];
+        if (port._side == "top" || port._side == "bottom") {
+            if (otherpt.x < thispt.x) {
+            return esl + 4 + idx * 8;
+            } else {
+            return esl + (count - idx - 1) * 8;
+            }
+        } else {  // left or right
+            if (otherpt.y < thispt.y) {
+            return esl + 4 + idx * 8;
+            } else {
+            return esl + (count - idx - 1) * 8;
+            }
+        }
+        }
+    }
+    return esl;
+    };
+
+    CustomLink.prototype.hasCurviness = function() {
+    if (isNaN(this.curviness)) return true;
+    return go.Link.prototype.hasCurviness.call(this);
+    };
+
+    CustomLink.prototype.computeCurviness = function() {
+    if (isNaN(this.curviness)) {
+        var fromnode = this.fromNode;
+        var fromport = this.fromPort;
+        var fromspot = this.computeSpot(true);
+        var frompt = fromport.getDocumentPoint(fromspot);
+        var tonode = this.toNode;
+        var toport = this.toPort;
+        var tospot = this.computeSpot(false);
+        var topt = toport.getDocumentPoint(tospot);
+        if (Math.abs(frompt.x - topt.x) > 20 || Math.abs(frompt.y - topt.y) > 20) {
+        if ((fromspot.equals(go.Spot.Left) || fromspot.equals(go.Spot.Right)) &&
+            (tospot.equals(go.Spot.Left) || tospot.equals(go.Spot.Right))) {
+            var fromseglen = this.computeEndSegmentLength(fromnode, fromport, fromspot, true);
+            var toseglen = this.computeEndSegmentLength(tonode, toport, tospot, false);
+            var c = (fromseglen - toseglen) / 2;
+            if (frompt.x + fromseglen >= topt.x - toseglen) {
+            if (frompt.y < topt.y) return c;
+            if (frompt.y > topt.y) return -c;
+            }
+        } else if ((fromspot.equals(go.Spot.Top) || fromspot.equals(go.Spot.Bottom)) &&
+            (tospot.equals(go.Spot.Top) || tospot.equals(go.Spot.Bottom))) {
+            var fromseglen = this.computeEndSegmentLength(fromnode, fromport, fromspot, true);
+            var toseglen = this.computeEndSegmentLength(tonode, toport, tospot, false);
+            var c = (fromseglen - toseglen) / 2;
+            if (frompt.x + fromseglen >= topt.x - toseglen) {
+            if (frompt.y < topt.y) return c;
+            if (frompt.y > topt.y) return -c;
+            }
+        }
+        }
+    }
+    return go.Link.prototype.computeCurviness.call(this);
+    };
+    // end CustomLink class
+
+
 Vue.component('physical-connections-viewer', {
     // The todo-item component now accepts a
     // "prop", which is like a custom attribute.
@@ -25,6 +116,9 @@ Vue.component('physical-connections-viewer', {
         landscape: function(lscape) {
 
             if ("pod" in lscape) {
+                
+                var portSize = new go.Size(16, 16);
+
                 var diagram = new go.Diagram("diagramConnections");
 
                 diagram.initialAutoScale = go.Diagram.Uniform;
@@ -37,16 +131,81 @@ Vue.component('physical-connections-viewer', {
                         "RoundedRectangle",  // use this kind of figure for the Shape
                         // bind Shape.fill to Node.data.color
                         new go.Binding("fill", "color")),
+
                     go.GraphObject.make(
                         go.TextBlock,
                         { margin: 3 , alignment: go.Spot.Bottom},  // some room around the text
                         // bind TextBlock.text to Node.data.key
                         new go.Binding("text", "key")),
+
                     go.GraphObject.make(
                         go.Picture,
-                        { desiredSize: new go.Size(100, 100) },
-                        new go.Binding("source", "icon"))
+                        { 
+                            desiredSize: new go.Size(100, 100)
+                        },
+                        new go.Binding("source", "icon")),
+
+                    go.GraphObject.make(
+                        go.Panel,
+                        "Horizontal",
+                        new go.Binding("itemArray", "topArray"),
+                        {
+                            row: 0,
+                            column: 1,
+                            itemTemplate: go.GraphObject.make(
+                                go.Panel,
+                                {
+                                    _side: "top",
+                                    fromSpot: go.Spot.Top, toSpot: go.Spot.Top,
+                                    fromLinkable: true, toLinkable: true, cursor: "pointer"
+                                    //contextMenu: portMenu
+                                },
+                                new go.Binding("portId", "portId"),
+                                go.GraphObject.make(
+                                    go.Shape,
+                                    "Rectangle",
+                                    {
+                                        stroke: null, strokeWidth: 0,
+                                        desiredSize: portSize,
+                                        margin: new go.Margin(0, 1)
+                                    },
+                                    new go.Binding("fill", "portColor")
+                                )
+                            )
+                        }
+                    ),
+
+                    go.GraphObject.make(
+                        go.Panel,
+                        "Horizontal",
+                        new go.Binding("itemArray", "bottomArray"),
+                        {
+                            row: 2,
+                            column: 1,
+                            itemTemplate: go.GraphObject.make(
+                                go.Panel,
+                                {
+                                    _side: "bottom",
+                                    fromSpot: go.Spot.Bottom, toSpot: go.Spot.Bottom,
+                                    fromLinkable: true, toLinkable: true, cursor: "pointer"
+                                    //contextMenu: portMenu
+                                },
+                                new go.Binding("portId", "portId"),
+                                go.GraphObject.make(
+                                    go.Shape,
+                                    "Rectangle",
+                                    {
+                                        stroke: null, strokeWidth: 0,
+                                        desiredSize: portSize,
+                                        margin: new go.Margin(0, 1)
+                                    },
+                                    new go.Binding("fill", "portColor")
+                                )
+                            )
+                        }
+                    ),
                 );
+
                 diagram.layout = go.GraphObject.make(
                     go.LayeredDigraphLayout,
                     {
@@ -55,6 +214,28 @@ Vue.component('physical-connections-viewer', {
                         columnSpacing: 15,
                         setsPortSpots: false
                     }
+                );
+
+                // an orthogonal link template, reshapable and relinkable
+                diagram.linkTemplate = go.GraphObject.make(
+                    CustomLink,
+                    {
+                        routing: go.Link.AvoidsNodes,
+                        corner: 4,
+                        curve: go.Link.JumpGap,
+                        reshapable: true,
+                        resegmentable: true,
+                        relinkableFrom: true,
+                        relinkableTo: true
+                    },
+                    new go.Binding("points").makeTwoWay(),
+                    go.GraphObject.make(
+                        go.Shape,
+                        {
+                            stroke: "#2F4F4F",
+                            strokeWidth: 2
+                        }
+                    )
                 );
 
                 var lookup_power_server = {};
