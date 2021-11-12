7cecf1aee55e76f01d51e7ea97921ecc62f5be55
Myron Walker <myron.walker@gmail.com>
2021-11-12 08:49:35 -0800

    Minor work on connecting testbed devices.

--- service/apod/website/static/components/physicalconnectionsviewer.js
+++ service/apod/website/static/components/physicalconnectionsviewer.js
@@ -47,41 +47,227 @@ Vue.component('physical-connections-viewer', {
                         { desiredSize: new go.Size(100, 100) },
                         new go.Binding("source", "icon"))
                 );
+                diagram.layout = go.GraphObject.make(
+                    go.LayeredDigraphLayout,
+                    {
+                        direction: 90,
+                        layerSpacing: 10,
+                        columnSpacing: 15,
+                        setsPortSpots: false
+                    }
+                );
+
+                var lookup_power_server = {};
+                var lookup_serial_server = {};
 
                 var graphNodes = [];
+                var graphLinks = [];
 
-                // Create all of the device nodes
-                for (const device of lscape.pod.devices) {
-                    var nodeInfo = null;
-                    iconUri = "static/images/unknown.png";
-                    if (device.cachedIcon != undefined) {
-                        iconUri = device.cachedIcon;
-                    }
-                    if (device.deviceType == "network/upnp") {
+                var nodeIndex = 0;
+
+                var linuxHostIconUri = "static/images/unknown.png";
+                var windowsHostIconUri = "static/images/unknown.png";
+                var unknownHostIconUri = "static/images/unknown.png";
+
+                var serialServerIconUri = "static/images/unknown.png";
+                var serialPowerIconUri = "static/images/unknown.png";
+
+                if (lscape.pod.power != undefined) {
+                    for (const pwrsvr of lscape.pod.power) {
+
                         nodeInfo = {
-                            "key": device.upnp.modelName,
-                            "icon": iconUri,
-                            "color": "lightgray"
+                            "key": pwrsvr.name,
+                            "iconUri": serialPowerIconUri,
+                            "color": "lightgray",
+                            "nodeIndex": nodeIndex
                         };
-                    }
-                    else if (device.deviceType == "network/ssh") {
-                        nodeInfo = {"key": device.host, "color": "pink"};
-                    }
-                    else {
-                        nodeInfo = {"key": "ERROR", "color": "pink"};
-                    }
 
-                    if (nodeInfo != null) {
-                        graphNodes.push(nodeInfo)
+                        lookup_power_server[pwrsvr.name] = nodeInfo
+
+                        pwrsvr.nodeIndex = nodeIndex;
+                        graphNodes.push(nodeInfo);
+                        nodeIndex += 1;
                     }
                 }
 
-                var graphLinks = [];
+                if (lscape.pod.serial != undefined) {
+                    for (const sersvr of lscape.pod.serial) {
 
-                diagram.model = new go.GraphLinksModel(
+                        nodeInfo = {
+                            "key": sersvr.name,
+                            "iconUri": serialServerIconUri,
+                            "color": "lightgray",
+                            "nodeIndex": nodeIndex
+                        };
+
+                        lookup_serial_server[sersvr.name] = nodeInfo
+
+                        sersvr.nodeIndex = nodeIndex;
+                        graphNodes.push(nodeInfo);
+                        nodeIndex += 1;
+                    }
+                }
+
+                if (lscape.pod.devices != undefined) {
+                    // Create all of the device nodes
+                    for (const device of lscape.pod.devices) {
+                        var nodeInfo = null;
+                        iconUri = "static/images/unknown.png";
+                        if (device.cachedIcon != undefined) {
+                            iconUri = device.cachedIcon;
+                        }
+                        if (device.deviceType == "network/upnp") {
+                            nodeInfo = {
+                                "key": device.upnp.modelName,
+                                "icon": iconUri,
+                                "color": "lightgray"
+                            };
+                        }
+                        else if (device.deviceType == "network/ssh") {
+                            nodeInfo = {
+                                "key": device.host,
+                                "iconUri": iconUri,
+                                "color": "pink"
+                            };
+                        }
+                        else {
+                            nodeInfo = {
+                                "key": "ERROR",
+                                "iconUri": iconUri,
+                                "color": "pink"
+                            };
+                        }
+
+                        if (device.features != undefined) {
+                            if(lscape.pod.power != undefined)
+                            {
+                                if (device.features.power != undefined) {
+                                    power_mapping = device.features.power;
+
+                                    pwrname = power_mapping.name;
+                                    pwrswitch = power_mapping.switch;
+
+                                    portId = pwrname + "/" + pwrswitch;
+                                    if ("bottomArray" in nodeInfo) {
+                                        nodeInfo["bottomArray"].push(
+                                            {
+                                                "portColor":"#ebe3fc",
+                                                "portId": portId
+                                            }
+                                        );
+                                    } else {
+                                        nodeInfo["bottomArray"] = [
+                                            {
+                                                "portColor":"#ebe3fc",
+                                                "portId": portId
+                                            }
+                                        ];
+                                    }
+
+                                    if (pwrname in lookup_power_server) {
+                                        pwrsvr = lookup_power_server[pwrname];
+                                        pwrsvridx = pwrsvr.nodeIndex;
+
+                                        if ("topArray" in pwrsvr) {
+                                            pwrsvr["topArray"].push(
+                                                {
+                                                    "portColor":"#ebe3fc",
+                                                    "portId": portId
+                                                }
+                                            );
+                                        } else {
+                                            pwrsvr["topArray"] = [
+                                                {
+                                                    "portColor":"#ebe3fc",
+                                                    "portId": portId
+                                                }
+                                            ]
+                                        }
+
+                                        linkNode = {
+                                            "from": pwrsvridx,
+                                            "to": nodeIndex,
+                                            "fromPort": pwrswitch,
+                                            "toPort": portId
+                                        };
+                                        graphLinks.push(linkNode)
+                                    }
+                                }
+                            }
+
+                            if(lscape.pod.serial != undefined)
+                            {
+                                if (device.features.serial != undefined) {
+                                    serial_mapping = device.features.serial;
+
+                                    sername = power_mapping.name;
+                                    serswitch = power_mapping.port;
+
+                                    portId = sername + "/" + serswitch;
+                                    if ("bottomArray" in nodeInfo) {
+                                        nodeInfo["bottomArray"].push(
+                                            {
+                                                "portColor":"#ebe3fc",
+                                                "portId": portId
+                                            }
+                                        );
+                                    } else {
+                                        nodeInfo["bottomArray"] = [
+                                            {
+                                                "portColor":"#ebe3fc",
+                                                "portId": portId
+                                            }
+                                        ];
+                                    }
+
+                                    if (sername in lookup_serial_server) {
+                                        sersvr = lookup_serial_server[sername];
+                                        sersvridx = sersvr.nodeIndex;
+
+                                        if ("topArray" in sersvr) {
+                                            sersvr["topArray"].push(
+                                                {
+                                                    "portColor":"#ebe3fc",
+                                                    "portId": portId
+                                                }
+                                            );
+                                        } else {
+                                            sersvr["topArray"] = [
+                                                {
+                                                    "portColor":"#ebe3fc",
+                                                    "portId": portId
+                                                }
+                                            ];
+                                        }
+
+                                        linkNode = {
+                                            "from": sersvridx,
+                                            "to": nodeIndex,
+                                            "fromPort": serswitch,
+                                            "toPort": portId
+                                        };
+                                        graphLinks.push(linkNode)
+                                    }
+                                }
+                            }
+                        }
+
+                        if (nodeInfo != null) {
+                            device.nodeIndex = nodeIndex;
+                            graphNodes.push(nodeInfo);
+                            nodeIndex += 1;
+                        }
+                    }
+                }
+
+                linkModel = new go.GraphLinksModel(
                     graphNodes,
                     graphLinks  // one link data, in an Array
                 );
+                linkModel.linkFromPortIdProperty = "fromPort";
+                linkModel.linkToPortIdProperty = "toPort";
+
+                diagram.model = linkModel
             }
         }
     },
